07988e95e5e26c35e833d0c027913ea0d4bad048
Rui Liu <fly_in_music@hotmail.com>
2023-01-06 15:17:26 +0100

    test remote

--- hello_world.py
+++ hello_world.py
@@ -8,7 +8,8 @@ def print_hello():
 
 dag = DAG('hello_Dags', description='Hello world Test',
           schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+          start_date=datetime(2017, 3, 20), catchup=False,
+          tags=["test", "world"])
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
