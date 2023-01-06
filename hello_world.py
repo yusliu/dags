231f29b81dd4ad65f0831f55e49f891b99929073
Rui Liu <fly_in_music@hotmail.com>
2023-01-06 15:45:08 +0100

    add tag

--- hello_world.py
+++ hello_world.py
@@ -9,7 +9,7 @@ def print_hello():
 dag = DAG('hello_Dags', description='Hello world Test',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False,
-          tags=["test", "rui"])
+          tags=["test", "rui","remote"])
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
